772d57a978762a7049b01e8a0f0314e9d25c9e09
tarumishra9498 <117687644+tarumishra9498@users.noreply.github.com>
2024-06-22 20:49:38 -0500

    Update content.js

--- content.js
+++ content.js
@@ -4,9 +4,9 @@ chrome.storage.local.get(['allergens', 'customAllergens'], (result) => {
     let foundAllergens = allergens.filter(allergen => pageContent.includes(allergen.toLowerCase()));
   
     if (foundAllergens.length > 0) {
-      alert('Allergens found on this page: ' + foundAllergens.join(', '));
+      alert('Restrictions found on this page: ' + foundAllergens.join(', '));
     } else {
-      alert('No allergens found on this page.');
+      alert('No restrictions found on this page.');
     }
   });
   
